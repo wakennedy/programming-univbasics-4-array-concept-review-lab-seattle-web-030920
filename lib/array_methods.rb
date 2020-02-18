28bade131226cc4a843546d8fdd88edbd98d57e2
William Kennedy <alexkenned@gmail.com>
2020-02-18 20:00:33 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -11,11 +11,10 @@ end
 
 def find_max_value(array)
   x = array[0] 
-  array.length.times { |index|
+  array.length.times do |index|
     if array[index] > x 
       x = array[index] 
     end
-  }
   x
   end
   
@@ -23,11 +22,10 @@ end
 
 def find_min_value(array)
   x = array[0] 
-  array.length.times { |index|
+  array.length.times do |index|
     if array[index] < x 
       x = array[index] 
     end
-  }
   x
   end
 end
